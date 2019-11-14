5fdfdac68fe02a87849cf2c4910897f978aa3f2b
shawinke <stijn.hawinkel@ugent.be>
2019-11-14 17:42:12 +0100

    Namechange test

--- tests/testthat/test-getApproxCovar.R
+++ tests/testthat/test-getApproxCovar.R
@@ -7,6 +7,6 @@ nBins = 80L
 reconsiObj = reconsi(Y = mat, x = x)
 
 test_that("Binned correlation matrix has the right dimensions", {
-    expect_equal(dim(reconsi:::getCperm(reconsiObj$statsPerm, nBins = nBins)),
+    expect_equal(dim(reconsi:::getApproxCovar(reconsiObj$statsPerm, nBins = nBins)),
         rep(nBins+2L, 2))
   })
