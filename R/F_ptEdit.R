e1bb6d49c83d0ed926e40f42bd0cb7cfc962ca61
shawinke <stijn.hawinkel@ugent.be>
2020-01-24 17:18:41 +0100

    With the newly named ptEdit

---
+++ R/F_ptEdit.R
@@ -0,0 +1,15 @@
+#' A custom function to calculate the distribution function of the t-test
+#'  statistic. For internal use only
+#'@importFrom stats pt qt
+#'@param q a vector with t-statistic and degrees of freedom
+#'@return A value between 0 and 1, the evaluation of the cdf
+ptEdit = function(q){
+  pt(q = q[1], df = q[2])
+}
+#' A custom function to calculate the quantile function of the t-test
+#'  statistic. For internal use only
+#'@param p a vector with quantile and degrees of freedom
+#'@return the corresponding quantile
+qtEdit = function(p){
+  qt(p = p[1], df = p[2])
+}
