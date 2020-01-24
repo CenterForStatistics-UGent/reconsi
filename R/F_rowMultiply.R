66c7c0e6d022b0676a12b6abbd8780c5f748e3e6
shawinke <stijn.hawinkel@ugent.be>
2020-01-24 17:02:34 +0100

    Details on rowMultiply

--- R/F_rowMultiply.R
+++ R/F_rowMultiply.R
@@ -4,7 +4,7 @@
 #' @param matrix a numeric matrix of dimension a-by-b
 #' @param vector a numeric vector of length b
 #'
-#' t(t(matrix)*vector) but then faster
+#' @Details t(t(matrix)*vector) but then faster
 #'
 #' @return a matrix, row multplied by the vector
 rowMultiply = function(matrix, vector){
