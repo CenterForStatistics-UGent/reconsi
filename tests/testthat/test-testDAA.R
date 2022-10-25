22e79445188bc5b44ad47b374688655c475590ed
stijn <stijn.hawinkel@psb.ugent.be>
2022-10-25 10:24:20 +0200

    Count matrix for testDAA

--- tests/testthat/test-testDAA.R
+++ tests/testthat/test-testDAA.R
@@ -2,9 +2,9 @@ context("input to testDAA")
 
 test_that("testDAA deals with all zero rows", {
     n = 20; p = 50; B = 1e2
-    mat = matrix(rnorm(n*p), n, p)
+    mat = matrix(rpois(n*p, lambda = 3), n, p)
     x = sample(c(0,1), n , replace = TRUE)
     FC = rpois(n, 1000)
     mat[1,] = 0
-    expect_error(testDAA(Y = mat, x = x, FC = FC), NA)
+    expect_no_error(testDAA(Y = mat, x = x, FC = FC))
 })
